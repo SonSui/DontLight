d158251f375accdf07376bafe90e8b4d0f445c7a
88814588nujam <97157138+88814588nujam@users.noreply.github.com>
2025-07-09 15:25:35 +0900

    modify

---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    private NetworkVariable<Vector3> position = new NetworkVariable<Vector3>();
+
+    void Update()
+    {
+        if (IsOwner)
+        {
+            // 本地控制逻辑
+            if (IsServer) position.Value = transform.position;
+            else SubmitPositionServerRpc(transform.position);
+        }
+        else
+        {
+            // 同步其他玩家位置
+            transform.position = position.Value;
+        }
+    }
+
+    [ServerRpc]
+    void SubmitPositionServerRpc(Vector3 newPosition)
+    {
+        position.Value = newPosition;
+    }
+}
